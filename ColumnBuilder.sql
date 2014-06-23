4a9830d11b5e148c34aee5991940533340053435
phersho <fernando.camacho.garza@gmail.com>
2014-06-23 18:50:19 -0500

    Fixings for column's builder script.

--- ColumnBuilder.sql
+++ ColumnBuilder.sql
@@ -1,19 +1,20 @@
 ﻿/*
- * Obtiene casi un script de creación de columnas en una tabla.
+ * Gets script for add columns.
  */
 
-select 'ALTER TABLE ' || 'order_type' || ' ADD COLUMN ' || column_name
-	|| ' ' || upper(data_type) || coalesce(' (' || coalesce(character_maximum_length, numeric_precision, datetime_precision) || ')', '')
-	|| (case is_nullable when 'NO' then ' NOT NULL' else '' end)
-	|| (case when column_default is not null then ' DEFAULT ' || column_default else '' end)
-	|| ';' as script
-	--, *
+select format('%s.%s.%s', table_schema, table_name, column_name) as "Column"
+    , format('ALTER TABLE %s.%s ADD COLUMN %s %s', table_schema, table_name, column_name, upper(data_type))
+    || coalesce(' (' || coalesce(character_maximum_length, numeric_precision, datetime_precision) || ')', '')
+    || (case is_nullable when 'NO' then ' NOT NULL' else '' end)
+    || (case when column_default is not null then ' DEFAULT ' || column_default else '' end)
+    || ';' as "AddColumn"
 
 from information_schema.columns
 
-where table_name = 'table' -- utilizar imaginación ;-)
-    --and column_name not in ('activated', 'code', 'created', 'created_by', 'deleted', 'description', 'name', 'type', 'updated', 'updated_by')
+where table_name = 'stocks' -- imagine ;-)
+    --and table_schema ilike 'schema'
+    --and column_name not in ('column')
 
-order by
-    column_name,
-    ordinal_position
+order by table_schema, table_name
+    --, ordinal_position
+    , column_name
